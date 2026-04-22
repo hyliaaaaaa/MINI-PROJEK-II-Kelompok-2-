34dc78267f45757db670fe2f18cef71aa608bbad
unknown <hanyemalia10@gmail.com>
2026-04-22 14:46:11 +0700

    hany mencoba

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from input_buku import input_petugas, input_buku
-from proses_buku import hitung_total, cari_terbanyak, hitung_rata_rata
+from proses_buku import hitung_total_halaman, cari_buku_terbanyak, hitung_rata_rata
+
 
 def main():
-    print("=== APLIKASI DATA BUKU ===\n")
+    print("=== APLIKASI DATA BUKU ===")
 
-    # input
-    nama = input_petugas()
+    nama_petugas = input_petugas()
     daftar_buku = input_buku()
 
-    # proses
-    total = hitung_total(daftar_buku)
-    terbanyak = cari_terbanyak(daftar_buku)
+    total = hitung_total_halaman(daftar_buku)
+    buku_terbanyak = cari_buku_terbanyak(daftar_buku)
     rata_rata = hitung_rata_rata(daftar_buku)
 
-    # output
     print("\n=== HASIL DATA ===")
-    print(f"Petugas       : {nama}")
+    print(f"Petugas: {nama_petugas}")
+
     print("\nDaftar Buku:")
-    
     for i, buku in enumerate(daftar_buku, start=1):
         print(f"{i}. {buku['judul']} ({buku['halaman']} halaman)")
 
-    print(f"\nTotal Halaman : {total}")
-    print(f"Rata-rata     : {rata_rata:.2f}")
-    print(f"Buku Terbanyak: {terbanyak['judul']} ({terbanyak['halaman']} halaman)")
+    print(f"\nTotal Halaman: {total}")
+    print(f"Rata-rata Halaman: {rata_rata:.2f}")
+    print(f"Buku dengan halaman terbanyak: {buku_terbanyak['judul']} ({buku_terbanyak['halaman']} halaman)")
+
 
 if __name__ == "_main_":
     main()
