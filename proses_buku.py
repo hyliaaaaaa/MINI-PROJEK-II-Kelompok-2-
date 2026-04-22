085401cf5e7c2f23877e187e4c516c0c658410ec
unknown <hanyemalia10@gmail.com>
2026-04-22 11:05:39 +0700

    Hany up kode ya

---
+++ proses_buku.py
@@ -0,0 +1,21 @@
+def hitung_total_halaman(daftar_buku):
+    total = 0
+    for buku in daftar_buku:
+        total += buku["halaman"]
+    return total
+
+
+def cari_buku_terbanyak(daftar_buku):
+    terbanyak = daftar_buku[0]
+
+    for buku in daftar_buku:
+        if buku["halaman"] > terbanyak["halaman"]:
+            terbanyak = buku
+
+    return terbanyak
+
+
+def hitung_rata_rata(daftar_buku):
+    total = hitung_total_halaman(daftar_buku)
+    rata = total / len(daftar_buku)
+    return rata
