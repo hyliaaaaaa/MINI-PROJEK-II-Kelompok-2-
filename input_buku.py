0d3090316782bf86b0abc1308c888694fd4b9448
sumiati <desabanuausang@gmail.com>
2026-04-22 12:40:09 +0700

    hany memperbaiki kode, pinjam laptop sumi wifi hany eror

--- input_buku.py
+++ input_buku.py
@@ -2,10 +2,13 @@ def input_petugas():
     nama = input("Masukkan nama petugas: ")
     return nama
 
+
 def input_buku():
     daftar = []
 
-    for i in range(3):
+    jumlah = int(input("Masukkan jumlah buku: "))
+
+    for i in range(jumlah):
         print(f"\nBuku ke-{i+1}")
         judul = input("Judul buku: ")
 
@@ -22,3 +25,18 @@ def input_buku():
         })
 
     return daftar
+
+
+def tampilkan_data(petugas, daftar_buku):
+    print("\n=== DATA PERPUSTAKAAN ===")
+    print("Nama Petugas:", petugas)
+    print("\nDaftar Buku:")
+
+    for i, buku in enumerate(daftar_buku, start=1):
+        print(f"{i}. {buku['judul']} - {buku['halaman']} halaman")
+
+
+# ===== PROGRAM UTAMA =====
+petugas = input_petugas()
+data_buku = input_buku()
+tampilkan_data(petugas, data_buku)
