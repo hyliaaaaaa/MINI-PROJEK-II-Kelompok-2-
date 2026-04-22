9502b5ada439411ace69779c60d0864982a654b9
rizka agustina <rizkaagustina@gmail.com>
2026-04-22 13:31:19 +0700

    sumi ganti kode

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
-def hitung_total(daftar_buku):
-    total = 0
-    for buku in daftar_buku:
-        total += buku["halaman"]
-    return total
+from input_buku import input_petugas, input_buku
+from proses_buku import hitung_total, cari_terbanyak, hitung_rata_rata
 
-<<<<<<< HEAD
 def main():
-    print("=== APLIKASI DATA BUKU ===")
-=======
-def cari_terbanyak(daftar_buku):
-    terbanyak = daftar_buku[0]
+    print("=== APLIKASI DATA BUKU ===\n")
 
-    for buku in daftar_buku:
-        if buku["halaman"] > terbanyak["halaman"]:
-            terbanyak = buku
->>>>>>> 66a8d77636355324a1b2fa26ab0e817fd0d96230
+    # input
+    nama = input_petugas()
+    daftar_buku = input_buku()
 
-    return terbanyak
-
-<<<<<<< HEAD
-    total = hitung_total_halaman(daftar_buku)
-    buku_terbanyak = cari_buku_terbanyak(daftar_buku)
+    # proses
+    total = hitung_total(daftar_buku)
+    terbanyak = cari_terbanyak(daftar_buku)
     rata_rata = hitung_rata_rata(daftar_buku)
 
+    # output
     print("\n=== HASIL DATA ===")
-    print(f"Petugas: {nama_petugas}")
-
+    print(f"Petugas       : {nama}")
     print("\nDaftar Buku:")
+    
     for i, buku in enumerate(daftar_buku, start=1):
         print(f"{i}. {buku['judul']} ({buku['halaman']} halaman)")
 
-    print(f"\nTotal Halaman: {total}")
-    print(f"Rata-rata Halaman: {rata_rata:.2f}")
-    print(f"Buku dengan halaman terbanyak: {buku_terbanyak['judul']} ({buku_terbanyak['halaman']} halaman)")
+    print(f"\nTotal Halaman : {total}")
+    print(f"Rata-rata     : {rata_rata:.2f}")
+    print(f"Buku Terbanyak: {terbanyak['judul']} ({terbanyak['halaman']} halaman)")
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
-=======
-def hitung_rata_rata(daftar_buku):
-    total = hitung_total(daftar_buku)
-    rata = total / len(daftar_buku)
-    return rata
->>>>>>> 66a8d77636355324a1b2fa26ab0e817fd0d96230
